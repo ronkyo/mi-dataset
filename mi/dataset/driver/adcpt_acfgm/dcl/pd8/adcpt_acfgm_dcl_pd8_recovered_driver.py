b02731eca4a52f89ceef84ff1b9aa75ad668b048
ronkyo <ronkyo@gmail.com>
2014-10-29 18:23:31 -0700

    Intial release for adcpt_pd8
    Change-Id: I8c1f24681981daa01686b38a6807eeee2f8834f2

---
+++ mi/dataset/driver/adcpt_acfgm/dcl/pd8/adcpt_acfgm_dcl_pd8_recovered_driver.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+# ##
+# OOIPLACEHOLDER
+#
+# Copyright 2014 Raytheon Co.
+##
+
+__author__ = "Jeff Roy"
+
+import os
+
+from mi.logging import config
+
+from mi.dataset.driver.adcpt_acfgm.dcl.pd8.adcpt_acfgm_dcl_pd8_driver_common import AdcptAcfgmDclPd8Driver
+from mi.dataset.dataset_parser import DataSetDriverConfigKeys
+
+
+def parse(basePythonCodePath, sourceFilePath, particleDataHdlrObj):
+
+    config.add_configuration(os.path.join(basePythonCodePath, 'res', 'config', 'mi-logging.yml'))
+
+    parser_config = {
+        DataSetDriverConfigKeys.PARTICLE_MODULE:  'mi.dataset.parser.adcpt_acfgm_dcl_pd8',
+        DataSetDriverConfigKeys.PARTICLE_CLASS: 'AdcptPd8ARecoveredInstrumentDataParticle',
+    }
+
+    driver = AdcptAcfgmDclPd8Driver(sourceFilePath, particleDataHdlrObj, parser_config)
+
+    return driver.process()
